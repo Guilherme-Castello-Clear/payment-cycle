542b20c22ebeb3278b8606ce3cf0e0c6792bbc32
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-12 08:23:58 -0300

    Feat: Redux configured

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render(){
@@ -10,9 +11,11 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0'></ContentHeader>
                 <Content>
-                    <ValueBox cols='12 4' color='green' value='R$10,00' text='Total de Créditos' icon='bank'/>
-                    <ValueBox cols='12 4' color='red' value='R$10,00' text='Total de Débitos' icon='credit-card'/>
-                    <ValueBox cols='12 4' color='blue' value='R$0,00' text='Valor Consolidado' icon='money'/>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' value='R$10,00' text='Total de Créditos' icon='bank'/>
+                        <ValueBox cols='12 4' color='red' value='R$10,00' text='Total de Débitos' icon='credit-card'/>
+                        <ValueBox cols='12 4' color='blue' value='R$0,00' text='Valor Consolidado' icon='money'/>
+                    </Row>
                 </Content>
             </div>
         )
