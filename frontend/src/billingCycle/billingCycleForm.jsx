66dcdcec9b811cbfd9045f0f0266151c90c0304d
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-14 12:34:42 -0300

    Feat: Added responsive summary

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,19 +6,30 @@ import { init } from './billingCycleActions';
 import labelAndInput from '../common/form/labelAndInput';
 import ItemList from './itemList'
 import { ready } from 'jquery';
+import Summary from './summary';
+
 
 class BillingCycleForm extends Component {
     
+    calculateSummary(){
+        const sum = (t, v) => t + v
+        return{
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
     render(){
 
         const { handleSubmit, readOnly, credits, debts } = this.props
-        console.log(credits)
+        const {sumOfCredits, sumOfDebts} = this.calculateSummary()
         return(
             <form role='form' onSubmit={handleSubmit}>
                 <div className="box-body">
                     <Field name='name' label='Nome' cols='12 4' placeholder='Infrome o nome' component={labelAndInput} readOnly={readOnly}></Field>
                     <Field name='month' label='Mês' cols='12 4' placeholder='Infrome o mês' type='number' component={labelAndInput} readOnly={readOnly}></Field>
                     <Field name='year' label='Ano' cols='12 4' placeholder='Infrome o ano' type='number' component={labelAndInput} readOnly={readOnly}></Field>
+                    <Summary credit={sumOfCredits} debt={sumOfDebts}></Summary>
                     <ItemList cols='12 6' list={credits} field='credits' legend='Créditos' readOnly={readOnly}></ItemList>
                     <ItemList cols='12 6' showStatus={true} list={debts} field='debts' legend='Débitos' readOnly={readOnly}></ItemList>
 
