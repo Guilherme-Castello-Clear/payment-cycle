0dfacc796f0272a0553284096873dfeecc25c462
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-17 09:31:44 -0300

    Feat: Improved CORS and Routes

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const auth = require('./auth')
 const BillingCycle = require('../api/billingCycle/billingCycleService')
 
-module.exports = function(server){
+module.exports = function (server) {
 
-    const router = express.Router()
-    server.use('/api', router)
+    //Protected
 
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    //Publics
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
